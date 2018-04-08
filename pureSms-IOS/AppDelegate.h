d85daf394166fbc6e5ef95df699282da83543c1b
xiongyc <971881086@qq.com>
2018-04-08 16:54:56 +0800

    初始化

---
+++ pureSms-IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pureSms-IOS
+//
+//  Created by YC X on 2018/4/8.
+//  Copyright © 2018年 YC X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
